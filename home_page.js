96eb758f8687c7f97b3c6e52761ee09ee6596df3
SK-Fast <thummajil25580@gmail.com>
2021-12-17 12:46:55 +0700

    Multiple news fetching!!

--- home_page.js
+++ home_page.js
@@ -1,21 +1,27 @@
+let colsm8 = document.getElementsByClassName("col-sm-8")[0]
+
+colsm8.innerHTML = "<h3>Feed</h3>" + colsm8.innerHTML
 
 
-fetch("https://raw.githubusercontent.com/SK-Fast/polyplus-other-stuff/main/announcement.json").then(datanonjson => datanonjson.json().then(data => {
+fetch("https://raw.githubusercontent.com/SK-Fast/polyplus-other-stuff/main/announcement.json").then(datanonjson => {console.log(datanonjson);datanonjson.json().then(data => {
     if (datanonjson.status !== 200) {
         return
     }
 
     if (data["HasAnnouncement"] == true) {
-        let AnnTemplate = `<div class="card" style="
+        console.log(data["News"])
+        data["News"].forEach(function(item) {
+            let AnnTemplate = `<div class="card" style="
         background: rgba(56,255,255,0%);
         border-color: rgba(56,255,255,0%);
-        ">
-        <a target="_none" href="${data["Link"]}">
-        <img title="${data["Title"]}" src="${data["Img"]}"></a>
+        "><center>
+        <a target="_none" href="${item["Link"]}">
+        <img style="width:80%" title="${item["Title"]}" src="${item["Img"]}"></a></center>
         </div>`
 
-        let colsm8 = document.getElementsByClassName("col-sm-8")[0]
 
+        
         colsm8.innerHTML = AnnTemplate + colsm8.innerHTML
+        })
     }
-}))
+})})
