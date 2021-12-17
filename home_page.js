d4759087cc754638f979c1d335fa9cc9105ae703
SK-Fast <thummajil25580@gmail.com>
2021-12-17 14:42:34 +0700

    Multiple News fetching

--- home_page.js
+++ home_page.js
@@ -2,6 +2,27 @@ let colsm8 = document.getElementsByClassName("col-sm-8")[0]
 
 colsm8.innerHTML = "<h3>Feed</h3>" + colsm8.innerHTML
 
+colsm8.innerHTML += `<style>
+.polyplus_card_feed img {
+    width:40%;
+    transition: 0.5s cubic-bezier(0.22, 1, 0.36, 1);
+    -moz-user-select: none;
+-webkit-user-select: none;
+user-select: none;
+pointer-events: none;
+
+}\n
+
+.polyplus_card_feed:hover img {
+    width:100%
+}\n
+
+.polyplus_card_feed:active img {
+    width:80%
+
+}\n
+</style>`
+
 
 fetch("https://raw.githubusercontent.com/SK-Fast/polyplus-other-stuff/main/announcement.json").then(datanonjson => {console.log(datanonjson);datanonjson.json().then(data => {
     if (datanonjson.status !== 200) {
@@ -14,9 +35,9 @@ fetch("https://raw.githubusercontent.com/SK-Fast/polyplus-other-stuff/main/annou
             let AnnTemplate = `<div class="card" style="
         background: rgba(56,255,255,0%);
         border-color: rgba(56,255,255,0%);
-        "><center>
-        <a target="_none" href="${item["Link"]}">
-        <img style="width:80%" title="${item["Title"]}" src="${item["Img"]}"></a></center>
+        ">
+        <a target="_none" class="polyplus_card_feed" href="${item["Link"]}">
+        <img title="${item["Title"]}" src="${item["Img"]}"></a>
         </div>`
 
 
