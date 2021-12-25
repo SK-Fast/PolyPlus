fdafbedeb005ea2a051c963787d41e99060981f0
SK-Fast <thummajil25580@gmail.com>
2021-12-25 21:32:16 +0700

    disable polytoria features css

--- scripts/theme/themeapplier.js
+++ scripts/theme/themeapplier.js
@@ -7,39 +7,49 @@ if (window.location.href == "https://polytoria.com/polyplus/settings/") {
         "bgimg_data",
         "usetheme",
         "navbarcolor",
-        "primarybgcolor"
+        "primarybgcolor",
+        "showchat"
     ]
             
     chrome.storage.sync.get(themedata, function(result) {
 
     
+        let processcss = ``
+
         if (result["usetheme"] == false) {
-            return
-        }
+        } else {
+            processcss = `
+            .navbar {
+                background-color: ${result["topbarcolor"]} !important;
+            }
     
-        let processcss = `
-        .navbar {
-            background-color: ${result["topbarcolor"]} !important;
+            .navbar-vertical {
+                background-color: ${result["navbarcolor"]} !important;
+            }
+    
+            body {
+                background-color: ${result["bgcolor"]} !important;
+                background-image: url("${result["bgimg_data"]}") !important;
+                background-repeat: ${result["bgtype"]} !important;
+                background-size: cover !important;
+                background-attachment: ${result["bgattachtype"]} !important;
+            }
+    
+            .btn-primary, .alert-primary {
+                background-color: ${result["primarybgcolor"]} !important;
+                border-width: 0px !important;
+            }
+    
+            `
         }
 
-        .navbar-vertical {
-            background-color: ${result["navbarcolor"]} !important;
+        if (result["showchat"] == false) {
+            processcss += `
+            #poly-chat-toggler, #poly-chat-container {
+                display: none !important;
+            }
+            `
         }
-
-        body {
-            background-color: ${result["bgcolor"]} !important;
-            background-image: url("${result["bgimg_data"]}") !important;
-            background-repeat: ${result["bgtype"]} !important;
-            background-size: cover !important;
-            background-attachment: ${result["bgattachtype"]} !important;
-        }
-
-        .btn-primary, .alert-primary {
-            background-color: ${result["primarybgcolor"]} !important;
-            border-width: 0px !important;
-        }
-
-        `
 
         var type = 'text/css';
         var file = new Blob([processcss], {
